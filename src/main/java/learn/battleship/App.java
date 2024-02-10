36b542efcece32b4e035b9d41a5c2062967a9c7d
Anthony Carroll <tc8appdevelo@gmail.com>
2024-02-10 11:02:00 -0600

    re arranged console output

--- src/main/java/learn/battleship/App.java
+++ src/main/java/learn/battleship/App.java
@@ -5,11 +5,12 @@ import java.util.Scanner;
 public class App {
     public static void main(String [] args) {
         Scanner console = new Scanner(System.in);
+        System.out.println();
         System.out.print("Enter game size: ");
         int n = console.nextInt();
         Game game = new Game(n);
         console.nextLine();
-
+        System.out.println();
         game.startGame();
 
         while (!game.isGameOver()) {
